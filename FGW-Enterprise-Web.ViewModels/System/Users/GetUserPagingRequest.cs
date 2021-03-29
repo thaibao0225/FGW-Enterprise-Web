8d3cc19e93d8ec857f68c7154c3081bf8691c8b5
thaibao0225 <thaibao0225@gmail.com>
2021-03-29 07:56:16 +0700

    Add project files.first commit

---
+++ FGW-Enterprise-Web.ViewModels/System/Users/GetUserPagingRequest.cs
@@ -0,0 +1,12 @@
+﻿using FGW_Enterprise_Web.ViewModels.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FGW_Enterprise_Web.ViewModels.System.Users
+{
+    public class GetUserPagingRequest: PagingRequestBase
+    {
+        public string Keyword { get; set; }
+    }
+}
